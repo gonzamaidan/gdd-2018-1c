a31b774dc79f71986849f028c733d1f3e7a12d87
nicomastroianni <nico.mastro@hotmail.com>
2018-05-19 19:42:07 -0300

    termino con las clases

--- FrbaHotel/Model/Habitaciones.cs
+++ FrbaHotel/Model/Habitaciones.cs
@@ -8,6 +8,46 @@ namespace FrbaHotel.Model
 {
     public class Habitaciones
     {
+        private int piso;
+        private bool estado;
+        private string ubicacion;
+        private int codigoTipoHabitacion;
 
+        public int getPiso()
+        {
+            return this.piso;
+
+        }
+        public void setPiso(int unPiso)
+        {
+            this.piso = unPiso;
+        }
+        public bool getEstado()
+        {
+            return this.estado;
+
+        }
+        public void setEstado(bool unEstado)
+        {
+            this.estado = unEstado;
+        }
+        public string getUbicacion()
+        {
+            return this.ubicacion;
+
+        }
+        public void setUbicacion(string unaUbicacion)
+        {
+            this.ubicacion = unaUbicacion;
+        }
+        public int getCodigoTipoHabitacion()
+        {
+            return this.codigoTipoHabitacion;
+
+        }
+        public void setCodigoTipoHabitacion(int unCodigoTipoHabitacion)
+        {
+            this.codigoTipoHabitacion = unCodigoTipoHabitacion;
+        }
     }
 }
---
+++ FrbaHotel/Model/Regimenes.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrbaHotel.Model
+{
+    public class Regimenes
+    {
+        private string descripcion;
+        private decimal precioDolares;
+        private bool estado;
+
+        public string getDescripcion()
+        {
+            return this.descripcion;
+
+        }
+        public void setDescripcion(String unaDescripcion)
+        {
+            this.descripcion = unaDescripcion;
+        }
+
+        public decimal getPrecioDolares()
+        {
+            return this.precioDolares;
+        }
+        public void setPrecioDolares(decimal unPrecioDolares)
+        {
+            this.precioDolares = unPrecioDolares;
+        }
+
+        public bool getEstado()
+        {
+            return this.estado;
+
+        }
+        public void setEstado(bool unEstado)
+        {
+            this.estado = unEstado;
+        }
+
+
+    }
+}
