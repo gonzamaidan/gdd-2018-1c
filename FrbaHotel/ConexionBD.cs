f12e1183ea835d417d916c9d84276199112b1244
Gonzalo Maidan <gonzalo_maidan@hotmail.com>
2018-06-03 21:27:24 -0300

    Agrego clase con conexion a base de datos

---
+++ FrbaHotel/ConexionBD.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrbaHotel
+{
+    class ConexionBD
+    {
+        public static SqlConnection conectar()
+        {
+            return new SqlConnection(Program.connectionString);
+        }
+
+    }
+}
