ad58a8d15f81c666fafba87d98d820ffc3088ab1
Gonzalo Maidan <gonzalo_maidan@hotmail.com>
2018-06-30 17:45:10 -0300

    Cambio texto de generar reserva

--- FrbaHotel/GenerarModificacionReserva/GenerarReservas.Designer.cs
+++ FrbaHotel/GenerarModificacionReserva/GenerarReservas.Designer.cs
@@ -175,9 +175,9 @@
             this.checkBox1.AutoSize = true;
             this.checkBox1.Location = new System.Drawing.Point(26, 304);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(85, 17);
+            this.checkBox1.Size = new System.Drawing.Size(98, 17);
             this.checkBox1.TabIndex = 17;
-            this.checkBox1.Text = "Con regimen";
+            this.checkBox1.Text = "Indicar regimen";
             this.checkBox1.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
             this.checkBox1.UseVisualStyleBackColor = true;
             this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
