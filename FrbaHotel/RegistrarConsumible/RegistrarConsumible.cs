ff1e00b22bd6bcdd4f8dd1573b88b3659188256c
Nacho <ignaciocordorusso@hotmail.com>
2018-07-08 16:55:49 -0300

    Agrego allinclusive

--- FrbaHotel/RegistrarConsumible/RegistrarConsumible.cs
+++ FrbaHotel/RegistrarConsumible/RegistrarConsumible.cs
@@ -15,12 +15,14 @@ namespace FrbaHotel.RegistrarConsumible
     {
         SqlConnection baseDeDatos;
         private string numeroDeReserva;
+        bool isAllInclusive;
 
         public RegistrarConsumible(string numeroReserva)
         {
             InitializeComponent();
             baseDeDatos = ConexionBD.conectar();
             this.dataGridView1.AllowUserToAddRows = false;
+            isAllInclusive = false;
             numeroDeReserva = numeroReserva;
             obtenerRegimenes();
         }
@@ -31,7 +33,11 @@ namespace FrbaHotel.RegistrarConsumible
             this.dataGridView1.Rows[this.dataGridView1.Rows.Count-1].Cells[0].Value = codigo;
             this.dataGridView1.Rows[this.dataGridView1.Rows.Count-1].Cells[1].Value = producto;
             this.dataGridView1.Rows[this.dataGridView1.Rows.Count-1].Cells[2].Value = precio;
-            this.dataGridView1.Rows[this.dataGridView1.Rows.Count - 1].Cells[3].Value = 0;         
+            this.dataGridView1.Rows[this.dataGridView1.Rows.Count-1].Cells[3].Value = 1;
+            if (isAllInclusive)
+            {
+                actualizarAllInclusive();
+            }
                     
         }
 
@@ -64,7 +70,7 @@ namespace FrbaHotel.RegistrarConsumible
                 MessageBox.Show(exc.Message);
                 throw new Exception();
             }
-
+            if (isAllInclusive)actualizarAllInclusive();
             SqlTransaction transaction = baseDeDatos.BeginTransaction();
             try
             {
@@ -90,7 +96,7 @@ namespace FrbaHotel.RegistrarConsumible
                 
                 foreach (DataGridViewRow row in this.dataGridView1.Rows)
                 {
-                    if(row.Index == 0)
+                    if(row.Index == 0 || row.Index == 1)
                     {
                         commandFactura = new SqlCommand("INSERT INTO LOS_MAGIOS.ITEM_FACTURA(NUMERO_FACTURA, CANTIDAD, PRECIO_UNIDAD"
                             + ") VALUES(@nroFactura, @cantidad, @precio)", baseDeDatos);
@@ -126,6 +132,7 @@ namespace FrbaHotel.RegistrarConsumible
             catch (Exception exc)
             {
                 MessageBox.Show(exc.Message);
+                Console.WriteLine(exc.Message + " " + exc.StackTrace);
                 transaction.Rollback();
             }
 
@@ -161,11 +168,25 @@ namespace FrbaHotel.RegistrarConsumible
             return total;
         }
 
+        private void actualizarAllInclusive()
+        {
+            decimal total = 0;
+            foreach (DataGridViewRow row in this.dataGridView1.Rows)
+            {
+                if (row.Index != 0 && row.Index != 1)
+                {
+                    total += Convert.ToDecimal(row.Cells[2].Value) * Convert.ToDecimal(row.Cells[3].Value);
+            
+                }
+            }
+            this.dataGridView1.Rows[1].Cells[2].Value = total * -1;
+        }
+
         private void obtenerRegimenes()
         {
             this.baseDeDatos.Open();
-            SqlCommand query = new SqlCommand("SELECT DESCRIPCION b, (SELECT DATEDIFF(day, FECHA_INGRESO, FECHA_EGRESO)+1 FROM LOS_MAGIOS.ESTADIAS WHERE CODIGO_RESERVA =@codReserva)*"+
-"PRECIO_DOLARES B, (SELECT COUNT(*) FROM LOS_MAGIOS.CLIENTES_POR_RESERVA WHERE CODIGO_RESERVA=@codReserva) FROM LOS_MAGIOS.REGIMENES b, LOS_MAGIOS.RESERVAS c WHERE c.CODIGO_RESERVA=@codReserva AND b.CODIGO_REGIMEN = c.CODIGO_REGIMEN", baseDeDatos);
+            SqlCommand query = new SqlCommand("SELECT b.DESCRIPCION, (SELECT DATEDIFF(day, FECHA_INGRESO, FECHA_EGRESO)+1 FROM LOS_MAGIOS.ESTADIAS WHERE CODIGO_RESERVA =@codReserva)*"+
+"PRECIO_DOLARES B, (SELECT COUNT(*) FROM LOS_MAGIOS.CLIENTES_POR_RESERVA WHERE CODIGO_RESERVA=@codReserva), b.CODIGO_REGIMEN FROM LOS_MAGIOS.REGIMENES b, LOS_MAGIOS.RESERVAS c WHERE c.CODIGO_RESERVA=@codReserva AND b.CODIGO_REGIMEN = c.CODIGO_REGIMEN", baseDeDatos);
             query.Parameters.Add("@codReserva", SqlDbType.Int);
             query.Parameters["@codReserva"].Value = numeroDeReserva;
             SqlDataReader reader = query.ExecuteReader();
@@ -175,6 +196,16 @@ namespace FrbaHotel.RegistrarConsumible
             this.dataGridView1.Rows[0].Cells[1].Value = reader[0].ToString();
             this.dataGridView1.Rows[0].Cells[2].Value = reader[1].ToString();
             this.dataGridView1.Rows[0].Cells[3].Value = reader[2].ToString();
+            if (Convert.ToInt32(reader[3])== 4)
+            {
+                isAllInclusive = true;
+                this.dataGridView1.Rows.Add();
+                this.dataGridView1.Rows[1].Cells[0].Value = numeroDeReserva;
+                this.dataGridView1.Rows[1].Cells[1].Value = "BENEFICIOS ALL INCLUSIVE";
+                this.dataGridView1.Rows[1].Cells[2].Value = 0;
+                this.dataGridView1.Rows[1].Cells[3].Value = 1;
+
+            }
             reader.Close();
             this.baseDeDatos.Close();
 
