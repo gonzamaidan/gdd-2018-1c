ca1cea0e3de6a855fc0a09815a2246344976e477
Gonzalo Maidan <gonzalo_maidan@hotmail.com>
2018-07-14 00:05:27 -0300

    Corrijo tabla de generar reservas

--- FrbaHotel/GenerarModificacionReserva/GenerarReservas.Designer.cs
+++ FrbaHotel/GenerarModificacionReserva/GenerarReservas.Designer.cs
@@ -124,6 +124,9 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(26, 373);
             this.dataGridView1.Name = "dataGridView1";
